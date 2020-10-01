b143d88517a17af22d3b1af5716aa893d09bcaf7
winflag10 <53182120+winflag10@users.noreply.github.com>
2020-10-01 13:46:33 +0100

    Colours, score and stacking

--- app.js
+++ app.js
@@ -1,50 +1,73 @@
+//Scene Setup
 var scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xf6ffe8 );
 
+//Camera Setup
 let aspect = window.innerWidth / window.innerHeight;
 let d=20
 var camera = new THREE.OrthographicCamera( - d * aspect, d * aspect, d, - d, -100, 1000);
+camera.position.set( 2, 2, 2 );
+camera.lookAt(new THREE.Vector3(0,0,0))
 
-var renderer = new THREE.WebGLRenderer({
-			antialias: true,
-			alpha: false
-		});
+//Renderer Setup
+var renderer = new THREE.WebGLRenderer({antialias: true,alpha: false});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-
-// ambient
+//Lighting
 scene.add( new THREE.AmbientLight( 0xffffff, 0.5 ) );
-
-// light
 var light = new THREE.PointLight( 0xffffff, 0.5 );
 light.position.set( 0, 499, 0 );
 scene.add( light );
 
+var stoppedBlocks = new THREE.Group();
+var currentBlock = null;
+var prevBlock = null;
+var id = 1;
 
 var geom = new THREE.BoxGeometry(5,1,5);
-var mat = new THREE.MeshToonMaterial({color:0x222736, shading: THREE.FlatShading})
+var mat = new THREE.MeshToonMaterial({color:0x222736, flatShading: true})
 var cube = new THREE.Mesh(geom, mat);
-scene.add(cube);
+stoppedBlocks.add(cube);
+scene.add(stoppedBlocks);
+
+document.onkeydown = function(e){
+	if(e.code == "Space"){
+		onactive()
+	}
+}
+
+document.onmousedown = function(e){
+	if(e.which == 1){// LEFT CLICK
+		onactive()
+	}
+}
+
+function onactive(){
+	let block = new Block(id);
+	if(currentBlock != null){
+
+		currentBlock.stop()
+
+		let stoppedBlocksPos = {y:stoppedBlocks.position.y};
+		let stoppedBlocksEndPos = {y:stoppedBlocks.position.y-1};
+		let stoppedBlocksTween = new TWEEN.Tween(stoppedBlocksPos)
+		.to(stoppedBlocksEndPos, 200)
+		.onUpdate(function(){stoppedBlocks.position.y = stoppedBlocksPos.y;})
+		.easing(TWEEN.Easing.Quadratic.Out);
+		stoppedBlocksTween.start();
+
+		prevBlock = currentBlock;
+	}else{
+		
+	}
+	currentBlock = block
+	document.getElementById("title").innerHTML = id
+	id++;
+}
 
 
-var geom = new THREE.BoxGeometry(5,1,5);
-var mat = new THREE.MeshToonMaterial({color:0x8f41b0, shading: THREE.FlatShading})
-var cube = new THREE.Mesh(geom, mat);
-cube.position.set(0,1,0)
-scene.add(cube);
 
-
-var position = { x : -8};
-var target = { x : 8};
-var tween = new TWEEN.Tween(position).to(target, 2000).onUpdate(function(){cube.position.x = position.x;}).easing(TWEEN.Easing.Sinusoidal.InOut)
-var tweenBack = new TWEEN.Tween(position).to({x: -8}, 2000).onUpdate(function(){cube.position.x = position.x;}).easing(TWEEN.Easing.Sinusoidal.InOut)
-tween.chain(tweenBack);
-tweenBack.chain(tween);
-tween.start();
-
-camera.position.set( 2, 2, 2 );
-camera.lookAt(new THREE.Vector3(0,0,0))
 
 function animate(){
 	requestAnimationFrame(animate);
--- modules/block.js
+++ modules/block.js
@@ -1,3 +1,66 @@
-class block{
+class Block{
+	constructor(id,x,z){
+		this.x = x;
+		this.z = z;
+		this.id = id;
 
+		if(id%2==0){
+			this.axis = "x";
+		}else{
+			this.axis = "z";
+		}
+
+		let offset = this.id;
+		let r = Math.sin(0.3 * offset) * 55 + 200;
+		let g = Math.sin(0.3 * offset + 2) * 55 + 200;
+		let b = Math.sin(0.3 * offset + 4) * 55 + 200;
+		this.color = new THREE.Color( r / 255, g / 255, b / 255 );
+
+		this.geom = new THREE.BoxGeometry(x,1,z);
+		this.mat = new THREE.MeshToonMaterial({color:this.color, flatShading: true})
+		var cube = new THREE.Mesh(this.geom, this.mat);
+		this.cube = cube
+
+		if(this.axis == "x"){
+			this.cube.position.set(-8,1,0)
+		}else{
+			this.cube.position.set(0,1,-8)
+		}
+		
+		scene.add(this.cube);
+
+		var startPos = { pos : -8};
+		var currentPos = { pos : -8};
+		var endPos = { pos : 8};
+
+		let speed = 2000 * Math.pow(0.98,id) * 2;
+
+		this.tween = new TWEEN.Tween(currentPos).to(endPos, speed)
+		.easing(TWEEN.Easing.Sinusoidal.InOut)
+
+		this.tweenBack = new TWEEN.Tween(currentPos).to(startPos, speed)
+		.easing(TWEEN.Easing.Sinusoidal.InOut)
+
+
+		if(this.axis == "x"){
+			this.tween.onUpdate(function(){cube.position.x = currentPos.pos;})
+			this.tweenBack.onUpdate(function(){cube.position.x = currentPos.pos;})
+		}else{
+			this.tween.onUpdate(function(){cube.position.z = currentPos.pos;})
+			this.tweenBack.onUpdate(function(){cube.position.z = currentPos.pos;})
+		}
+
+		this.tween.chain(this.tweenBack);
+		this.tweenBack.chain(this.tween);
+		this.tween.start();
+	}
+
+	stop(){
+		this.tween.stop();
+		this.cube.position.y = -stoppedBlocks.position.y+1
+		this.cube.position.z = Math.round(this.cube.position.z*4)/4
+		this.cube.position.x = Math.round(this.cube.position.x*4)/4
+		stoppedBlocks.add(this.cube);
+	}
 }
+
